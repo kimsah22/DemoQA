59280739d98e5ec98d3b7a2979093eae7e8c557e
Kim Gan <h.b.yeganyan@gmail.com>
2025-04-06 22:25:08 +0400

    My first experience in cypress, I have deleted some spaces in the demoQA.cy.js file.

--- cypress/e2e/1-getting-started/demoQA.cy.js
+++ cypress/e2e/1-getting-started/demoQA.cy.js
@@ -1,5 +1,3 @@
-
-
 describe('Automation Practice Form', () => {
     beforeEach(() => {
         cy.clearCookies();
@@ -15,17 +13,14 @@ describe('Automation Practice Form', () => {
         cy.get('#userEmail').type('test@gmail.com');
         cy.get('[for="gender-radio-2"]').click(); 
         cy.get('#userNumber').type('1234567890');
-           
         cy.get('[for="hobbies-checkbox-3"]').click(); 
         cy.get('#currentAddress').type('Ankakhutyan str');
         cy.get('#submit').click();
-        
+
         cy.contains('Thanks for submitting the form').should('be.visible');
         //cy.get('#closeLargeModal').click();
         cy.wait(1000)
         cy.get('#closeLargeModal').click({ force: true }); 
-        
-        
-        
+    
     });
   });
